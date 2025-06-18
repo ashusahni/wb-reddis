d5e499217c8547e9b105ad5c5a8d0c3a3658ba15
ashutosh <ashutoshsahni833@gmail.com>
2025-06-18 23:34:02 +0530

    middleware

--- preq/middleware.ts
+++ preq/middleware.ts
@@ -1,14 +1,14 @@
-import { Playwrite_BE_VLG } from "next/font/google";
-import { NextRequest, NextResponse } from "next/server";
+// import { Playwrite_BE_VLG } from "next/font/google";
+// import { NextRequest, NextResponse } from "next/server";
 
-let reqCount = 0;
-export function middleware (req: NextRequest,){
- reqCount++;
- const res = NextResponse.next()
- console.log("recount is ",reqCount)
- return res;
-}
+// let reqCount = 0;
+// export function middleware (req: NextRequest,){
+//  reqCount++;
+//  const res = NextResponse.next()
+//  console.log("recount is ",reqCount)
+//  return res;
+// }
 
-export const config={
-    matcher: "/api/:path*",
-}
+// export const config={
+//     matcher: "/api/:path*",
+// }
